75ed6ab9f2805e45aca9ed4f189a12e8d7b33f19
David Hough <davidhough86@gmail.com>
2015-05-20 13:34:30 +0100

    Browser count aggregation

--- src/main/scala/com/boldradius/sdf/akka/UserStatisticsActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/UserStatisticsActor.scala
@@ -1,7 +1,6 @@
 package com.boldradius.sdf.akka
 
 import akka.actor.{Props, Actor, ActorLogging}
-import com.boldradius.sdf.akka.UserStatisticsActor.{BrowserUsersAggregation, Aggregation, AggregatorFn}
 
 
 //case class Request(sessionId: Long, timestamp: Long, url: String, referrer: String, browser: String)
@@ -10,21 +9,24 @@ class UserStatisticsActor extends Actor with ActorLogging {
 
   private var allRequests: List[Request] = List()
 
-  private var aggregations: List[Aggregation] = List.empty
-
+  // Aggregations
   private var requestsPerBrowser: Map[String, Int]  = Map().withDefaultValue(0)
 
-
-
   override def receive: Receive = {
-    case SessionHandlingActor.Requests(listRequest) =>
-      allRequests = allRequests ::: listRequest
-      val aggregation = BrowserUsersAggregation()
-      listRequest.groupBy(_.browser).map(tuple => (tuple._1, tuple._2.size)) foreach {
-        case (browser, newNum) =>
-          val oldNum = aggregation.value(browser)
-          requestsPerBrowser += browser -> (oldNum + newNum)
-      }
+    case SessionHandlingActor.Requests(requests) =>
+      allRequests = allRequests ::: requests
+      browserUsersAggregation(requests)
+  }
+
+  def browserUsersAggregation(requests: List[Request]) = {
+    val newBrowserAggregation = requests.groupBy(_.browser).map { case (browser, reqs) =>
+      browser -> reqs.size
+    }
+
+    newBrowserAggregation.foreach { case (browser, count) =>
+        val oldCount = requestsPerBrowser(browser)
+        requestsPerBrowser += browser -> (oldCount + count)
+    }
   }
 
 
@@ -32,9 +34,4 @@ class UserStatisticsActor extends Actor with ActorLogging {
 
 object UserStatisticsActor {
   def props: Props = Props[UserStatisticsActor]
-
-  sealed trait Aggregation
-  case class BrowserUsersAggregation(value: Map[String, Int] = Map.empty.withDefaultValue(0)) {
-    override def toString = "Users per browser: \n" + value.mkString("\n")
-  }
 }
