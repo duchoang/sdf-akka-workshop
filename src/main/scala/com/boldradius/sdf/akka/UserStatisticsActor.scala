4912b75169ddd6d1c24f43590bbf4643da3369cf
David Hough <davidhough86@gmail.com>
2015-05-20 13:19:57 +0100

    Aggregations

--- src/main/scala/com/boldradius/sdf/akka/UserStatisticsActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/UserStatisticsActor.scala
@@ -1,6 +1,7 @@
 package com.boldradius.sdf.akka
 
 import akka.actor.{Props, Actor, ActorLogging}
+import com.boldradius.sdf.akka.UserStatisticsActor.{BrowserUsersAggregation, Aggregation, AggregatorFn}
 
 
 //case class Request(sessionId: Long, timestamp: Long, url: String, referrer: String, browser: String)
@@ -9,15 +10,19 @@ class UserStatisticsActor extends Actor with ActorLogging {
 
   private var allRequests: List[Request] = List()
 
+  private var aggregations: List[Aggregation] = List.empty
+
   private var requestsPerBrowser: Map[String, Int]  = Map().withDefaultValue(0)
+
 
 
   override def receive: Receive = {
     case SessionHandlingActor.Requests(listRequest) =>
       allRequests = allRequests ::: listRequest
+      val aggregation = BrowserUsersAggregation()
       listRequest.groupBy(_.browser).map(tuple => (tuple._1, tuple._2.size)) foreach {
         case (browser, newNum) =>
-          val oldNum = requestsPerBrowser(browser)
+          val oldNum = aggregation.value(browser)
           requestsPerBrowser += browser -> (oldNum + newNum)
       }
   }
@@ -27,4 +32,9 @@ class UserStatisticsActor extends Actor with ActorLogging {
 
 object UserStatisticsActor {
   def props: Props = Props[UserStatisticsActor]
+
+  sealed trait Aggregation
+  case class BrowserUsersAggregation(value: Map[String, Int] = Map.empty.withDefaultValue(0)) {
+    override def toString = "Users per browser: \n" + value.mkString("\n")
+  }
 }
