7707f42dc8381d4c98ad78eb58a325d0bc9a80ac
David Hough <davidhough86@gmail.com>
2015-05-20 14:19:29 +0100

    Aggregation of page visits

--- src/main/scala/com/boldradius/sdf/akka/UserStatisticsActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/UserStatisticsActor.scala
@@ -1,6 +1,7 @@
 package com.boldradius.sdf.akka
 
 import akka.actor.{Props, Actor, ActorLogging}
+import com.boldradius.sdf.akka.UserStatisticsActor.Percent
 
 
 //case class Request(sessionId: Long, timestamp: Long, url: String, referrer: String, browser: String)
@@ -11,11 +12,13 @@ class UserStatisticsActor extends Actor with ActorLogging {
 
   // Aggregations
   private var requestsPerBrowser: Map[String, Int] = Map().withDefaultValue(0)
+  private var requestsPerPage: Map[String, Percent] = Map.empty
 
   override def receive: Receive = {
     case SessionHandlingActor.Requests(requests) =>
       allRequests = allRequests ::: requests
       browserUsersAggregation(requests)
+      pageVisitsAggregation(allRequests)
   }
 
   def browserUsersAggregation(requests: List[Request]): Map[String, Int] = {
@@ -30,9 +33,21 @@ class UserStatisticsActor extends Actor with ActorLogging {
     requestsPerBrowser
   }
 
+  def pageVisitsAggregation(requests: List[Request]): Map[String, Percent] = {
+    val totalCount = requests.size.toDouble
+    requestsPerPage ++= requests.groupBy(_.url).map { case (url, reqs) =>
+      url -> Percent(reqs.size / totalCount * 100)
+    }
+    requestsPerPage
+  }
+
 
 }
 
 object UserStatisticsActor {
   def props: Props = Props[UserStatisticsActor]
+
+  case class Percent(percent: Double) {
+    override def toString = f"$percent%.2f"
+  }
 }
--- src/test/scala/com/boldradius/sdf/akka/UserStatisticsActorSpec.scala
+++ src/test/scala/com/boldradius/sdf/akka/UserStatisticsActorSpec.scala
@@ -1,6 +1,7 @@
 package com.boldradius.sdf.akka
 
 import akka.testkit.TestActorRef
+import com.boldradius.sdf.akka.UserStatisticsActor.Percent
 
 class UserStatisticsActorSpec extends BaseAkkaSpec {
 
@@ -12,9 +13,19 @@ class UserStatisticsActorSpec extends BaseAkkaSpec {
       )
       result shouldEqual Map("Chrome" -> 3, "Firefox" -> 1, "IE10" -> 1)
     }
+
+    "Count percentage of requests by page" in {
+      val userStatsRef = TestActorRef(new UserStatisticsActor)
+      val requests = List(testChromeRequest, testChromeRequest, testChromeRequest, testOtherPageRequest, testOtherPageRequest)
+      val result = userStatsRef.underlyingActor.pageVisitsAggregation(requests)
+      result shouldEqual Map("test.org" -> Percent(60), "test.org/index.html" -> Percent(40))
+      val result2 = userStatsRef.underlyingActor.pageVisitsAggregation(testOtherPageRequest :: requests)
+      result2 shouldEqual Map("test.org" -> Percent(50), "test.org/index.html" -> Percent(50))
+    }
   }
 
   val testChromeRequest = Request(42l, 42l, "test.org", "", "Chrome")
   val testFirefoxRequest = testChromeRequest.copy(browser = "Firefox")
   val testIERequest = testChromeRequest.copy(browser = "IE10")
+  val testOtherPageRequest = testChromeRequest.copy(url = "test.org/index.html")
 }
